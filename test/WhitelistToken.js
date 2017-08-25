ca79f491aefd9b9f81d6810eceb6603031bdf285
Rudy Godoy <rudygodoyg@gmail.com>
2017-08-24 23:13:22 -0500

    Added tests for external contract calls.

--- test/WhitelistToken.js
+++ test/WhitelistToken.js
@@ -1,14 +1,17 @@
 const assertJump = require('./assertJump');
 var WhitelistToken = artifacts.require("../contracts/WhitelistToken.sol");
+var TokenHolder = artifacts.require("../contracts/TokenHolder.sol");
 
 contract('WhitelistToken', function(accounts) {
   var token;
+  var holder;
   var owner = accounts[0];
   var account_one = accounts[1];
   var account_two = accounts[2];
   
   beforeEach(async function() {
     token = await WhitelistToken.new(owner, 100);
+    holder = await TokenHolder.new(token.address, {from: account_one});
   });
   
   it("should return the correct totalSupply after construction", async function() {
@@ -48,5 +51,26 @@ contract('WhitelistToken', function(accounts) {
     assert.fail('should have thrown before');
   });
 
+  it("should match token.address with contract's tokenAddress", async function() {
+    let holderTokenAddr = await holder.tokenAddress();
+    assert.equal(token.address, holderTokenAddr);
+  });
+
+  it("should have correct balance after transfer", async function() {
+    await token.enableWhitelist(holder.address);
+    await token.transfer(holder.address, 50);
+    let holderBalance = await token.balanceOf(holder.address);
+    let holderContractBal = await holder.bal();
+    assert.equal(holderBalance.toNumber(), holderContractBal.toNumber());
+  });
+
+  it("should allow transfer of funds from whitelisted contract", async function() {
+    await token.enableWhitelist(holder.address);
+    await token.transfer(holder.address, 50);
+    await holder.transferToken(owner, 15, {from: account_one});
+    let ownerBal = await token.balanceOf(owner);
+    let holderBal = await token.balanceOf(holder.address);
+    assert.equal(ownerBal.toNumber() - 15, holderBal.toNumber() + 15);
+  });
 });
 
